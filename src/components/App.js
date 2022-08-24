2d2b7bfc18d12e66cffcd81bea92a9a9aef062a7
Lakshya Sharma <111343896+lakshya317@users.noreply.github.com>
2022-08-25 01:10:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Landing from "./Landing/Landing";
 import Login from "./Login/Login";
 import Dashboard from "./Dashboard/Dashboard";
-import Home from "./Home/Home";
+//import Home from "./Home/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageNotFound from "./PageNotFound/PageNotFound";
 
